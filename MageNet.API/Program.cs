ddec1d256d4a556c8a8ac9a54b959dd0a737faed
Aleh Kasiankou <aleh.kasiankou@gmail.com>
2022-12-30 15:29:43 +0300

    Added Meta controller (with attr types) + fixed CORS policy

--- MageNet.API/Program.cs
+++ MageNet.API/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using MageNet.Persistence;
 using MageNetServices.DI;
 using Microsoft.EntityFrameworkCore;
@@ -6,13 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(opts =>
+{
+    var enumConverter = new JsonStringEnumConverter();
+    opts.JsonSerializerOptions.Converters.Add(enumConverter);
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.RegisterDbContext(builder.Configuration.GetConnectionString("default"));
 builder.Services.RegisterRepositoryServices();
 builder.Services.RegisterValidationServices();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "apiCompatibilityPolicy",
+        policy => { policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); });
+});
 
 var app = builder.Build();
 
@@ -21,7 +32,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Ubuntu"))
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    using(var scope = app.Services.CreateScope())
+    using (var scope = app.Services.CreateScope())
     {
         var mageNetContext = scope.ServiceProvider.GetRequiredService<MageNetDbContext>();
         mageNetContext.Database.Migrate();
@@ -32,6 +43,8 @@ else
 {
     app.UseHttpsRedirection();
 }
+
+app.UseCors("apiCompatibilityPolicy");
 
 app.UseAuthorization();
 
